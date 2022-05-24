1e79318e5b20a92f9bbb4e1bef20ebe7210c9630
Illia Kononenko <ilyanes88@gmail.com>
2022-05-25 00:18:46 +0300

    last

--- src/App.js
+++ src/App.js
@@ -1,7 +1,59 @@
-import "./App.css";
+import { Component } from "react";
+import { Container, StyleNoFeedback, TitleStatics } from "./App.styled";
+import { Buttons } from "./components/Buttons/Buttons";
+import { Statistics } from "./components/Statistics/Statistics";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  state = {
+    Good: 0,
+    Neutral: 0,
+    Bad: 0,
+  };
+  allButtons = Object.keys(this.state);
+  staticData = {};
+  handleClick = (e) => {
+    const { name } = e.target;
+    this.setState((prevState) => ({
+      [name]: prevState[name] + 1,
+    }));
+  };
+  render() {
+    const values = Object.values(this.state);
+
+    const countTotalFeedback = values.reduce((acc, value) => {
+      return acc + value;
+    }, 0);
+
+    const countPositiveFeedbackPercentage =
+      (this.state.Good / countTotalFeedback) * 100;
+
+    this.staticData = {
+      ...this.state,
+      ...{
+        total: countTotalFeedback,
+        positive: countPositiveFeedbackPercentage,
+      },
+    };
+
+    return (
+      <Container>
+        <h1>Please leave feedback</h1>
+        <Buttons buttons={this.allButtons} onClick={this.handleClick}></Buttons>
+        <TitleStatics>
+          Statistics
+          {countTotalFeedback !== 0 ? (
+            <Statistics
+              value={countPositiveFeedbackPercentage}
+              total={countTotalFeedback}
+              data={this.staticData}
+            ></Statistics>
+          ) : (
+            <StyleNoFeedback>No feedback given</StyleNoFeedback>
+          )}
+        </TitleStatics>
+      </Container>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Buttons/Buttons.jsx
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+export const Buttons = ({ buttons, onClick }) => {
+  return (
+    <>
+      {buttons.map((button) => {
+        return (
+          <button key={button} onClick={onClick} name={button}>
+            {button}
+          </button>
+        );
+      })}
+    </>
+  );
+};
+Buttons.propTypes = {
+  buttons: PropTypes.array,
+};
